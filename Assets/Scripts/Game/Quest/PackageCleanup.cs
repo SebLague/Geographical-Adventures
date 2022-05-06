541740909ae705f5c4aa363888b274ff86131945
Pattrigue <57709490+Pattrigue@users.noreply.github.com>
2022-05-06 23:54:08 +0200

    Update PackageCleanup.cs
    Changed the code style to match the rest of the project

--- Assets/Scripts/Game/Quest/PackageCleanup.cs
+++ Assets/Scripts/Game/Quest/PackageCleanup.cs
@@ -4,16 +4,16 @@ namespace GeoGame.Quest
 {
     public class PackageCleanup : MonoBehaviour
     {
-        private QuestSystem questSystem;
-        private PackageDropInfo lastPackageDropInfo;
+        QuestSystem questSystem;
+        PackageDropInfo lastPackageDropInfo;
 
-        private void Awake() => questSystem = GetComponent<QuestSystem>();
+        void Awake() => questSystem = GetComponent<QuestSystem>();
 
-        private void OnEnable() => questSystem.packageDropped += OnPackageDropped;
+        void OnEnable() => questSystem.packageDropped += OnPackageDropped;
 
-        private void OnDisable() => questSystem.packageDropped -= OnPackageDropped;
+        void OnDisable() => questSystem.packageDropped -= OnPackageDropped;
 
-        private void OnPackageDropped(PackageDropInfo packageDropInfo)
+        void OnPackageDropped(PackageDropInfo packageDropInfo)
         {
             lastPackageDropInfo?.Destroy();
             lastPackageDropInfo = packageDropInfo;
