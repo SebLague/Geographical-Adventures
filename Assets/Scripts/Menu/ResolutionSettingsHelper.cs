78c5d04ad81037eadcee9be1bbc11038333254aa
Sebastian Lague <sebastlague@gmail.com>
2022-05-11 11:54:56 +0200

    First-launch window resolution threshold
    Restrict game resolution on first launch as temp work-around for perf issues with rendering at high resolutions

---
+++ Assets/Scripts/Menu/ResolutionSettingsHelper.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ResolutionSettingsHelper
+{
+
+	public static Vector2Int GetRatio(int width, int height)
+	{
+		int gcd = Maths.Other.GreatestCommonDivisor(width, height);
+		int aspectW = width / gcd;
+		int aspectH = height / gcd;
+		Vector2Int aspect = new Vector2Int(aspectW, aspectH);
+		return aspect;
+	}
+
+	public static Vector2Int GetRatio(Resolution res)
+	{
+		return GetRatio(res.width, res.height);
+	}
+
+	public static string ResolutionToName(Resolution res)
+	{
+		Vector2Int aspectRatio = GetRatio(res);
+		// Change 8:5 to 16:10 since that's typically used (presumably to compare easier to 16:9)
+		if (aspectRatio.x == 8 && aspectRatio.y == 5)
+		{
+			aspectRatio *= 2;
+		}
+		return $"{res.width} x {res.height} ({aspectRatio.x}:{aspectRatio.y})";
+	}
+
+}
